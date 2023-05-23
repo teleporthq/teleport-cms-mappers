bafdd708b95867e631adadb90969dcc4d137e785
Ionut Pasca <pascaionut94@gmail.com>
2023-05-23 11:40:27 +0300

    Add wordpress mapper

---
+++ src/wordpress/index.ts
@@ -0,0 +1,24 @@
+export const normalize = (input) => {
+  if (
+    input === null ||
+    input === undefined ||
+    (typeof input === 'object' && !Object.keys(input).length)
+  ) {
+    return null
+  }
+
+  if (Array.isArray(input)) {
+    return input.map(normalize)
+  }
+
+  const output = { ...input } as Record<string, Record<string, unknown>>
+  if (output?.title?.rendered) {
+    try {
+    const parser = new DOMParser()
+    const newTitle = parser.parseFromString(output?.title?.rendered || '', 'text/html')
+    output.title.rendered = newTitle?.documentElement?.textContent || output.title.rendered
+    } catch(error) {}
+  }
+
+  return output
+}
