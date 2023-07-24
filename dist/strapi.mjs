9f611e3cb0ac07e238718992d6cc2fc4cdbf3d5d
Tudor <tudor.cernusca@teleporthq.io>
2023-07-24 15:49:57 +0300

    Add build too

--- dist/strapi.mjs
+++ dist/strapi.mjs
@@ -2,19 +2,19 @@ function l(a) {
   if (typeof a != "object" || a === null)
     return a;
   const t = {};
-  for (const i in a)
-    t[i] = l(a[i]);
+  for (const e in a)
+    t[e] = l(a[e]);
   return t;
 }
 const s = (a) => {
   const t = {};
-  for (const i in a) {
-    const e = a[i];
-    if (typeof e == "object" && e !== null && "data" in e && e.data !== null && "id" in e.data && "attributes" in e.data) {
-      const o = r(e);
-      t[i] = { id: e.data.id, ...o };
+  for (const e in a) {
+    const i = a[e];
+    if (typeof i == "object" && i !== null && "data" in i && i.data !== null && "id" in i.data && "attributes" in i.data) {
+      const o = r(i);
+      t[e] = { id: i.data.id, ...o };
     } else
-      t[i] = l(e);
+      t[e] = l(i);
   }
   return t;
 }, r = (a) => {
@@ -22,26 +22,28 @@ const s = (a) => {
     return null;
   if (Array.isArray(a))
     return {
-      data: a.map(n)
+      data: a.map(d)
     };
   let t = { ...a };
   return a.attributes && (t = {
     ...t,
     ...s(a.attributes)
   }, delete t.attributes), a.data && (t = r(a.data)), t;
-}, n = (a) => ({
-  meta: {
-    ...a == null ? void 0 : a.meta,
-    pagination: {
-      total: a.meta.pagination.total,
-      limit: a.meta.pagination.limit,
-      start: a.meta.pagination.start,
-      pages: a.meta.pagination.total / a.meta.pagination.limit
-    }
-  },
-  ...r(a)
-});
+}, d = (a) => {
+  var e;
+  let t;
+  return a.meta.pagination.total && a.meta.pagination.limit && (t = Math.ceil(a.meta.pagination.total / a.meta.pagination.limit)), {
+    meta: {
+      ...a == null ? void 0 : a.meta,
+      pagination: {
+        ...(e = a == null ? void 0 : a.meta) == null ? void 0 : e.pagination,
+        pages: t
+      }
+    },
+    ...r(a)
+  };
+};
 export {
-  n as normalize,
+  d as normalize,
   r as normalizeContent
 };
